99265f036abc103b47b007ffc753ff4d2028d082
PNS212624\Hansol <yrlee00@hansol.com>
2025-01-06 13:42:46 +0900

    feat: 주문 내역 구현 완료

--- src/app/orders/_components/Order.tsx
+++ src/app/orders/_components/Order.tsx
@@ -1,9 +1,17 @@
 import OrderSearch from '@/app/orders/_components/OrderSearch'
+import OrderList from '@/app/orders/_components/OrderList'
 
 const Order = () => {
   return (
     <div className="flex h-full flex-col gap-3 px-mobile_safe">
-      <OrderSearch />
+      <div className="sticky top-0 z-20 bg-white">
+        <OrderSearch />
+      </div>
+      <OrderList />
+      <OrderList />
+      <OrderList />
+      <OrderList />
+      <OrderList />
     </div>
   )
 }
--- src/app/orders/_components/OrderSearch.tsx
+++ src/app/orders/_components/OrderSearch.tsx
@@ -1,10 +1,18 @@
-import search from '@/assets/icons/search.svg'
+import { COLORS } from '@/styles/color'
+import Icon from '@/components/Icon'
 
 const OrderSearch = () => {
   return (
-    <div className="pt-2">
+    <div className="relative pt-2">
+      <Icon
+        className="absolute left-3 top-5"
+        variant="search"
+        width={14}
+        height={14}
+        fill={COLORS.gray600}
+      />
       <input
-        className="border-gray-300 border-2 rounded-md w-full p-2"
+        className="w-full rounded-md border-2 border-gray-300 p-2 pl-8"
         type="search"
         placeholder="주문 내역을 검색하세요"
       />
