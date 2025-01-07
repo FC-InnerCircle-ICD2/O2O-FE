6e739595c7cf7795d1e3176caa7c5f7c23a0f6b5
Suhyeon Jeon <suhyeon.jeon@fig.xyz>
2025-01-07 23:39:27 +0900

    Fix: splash 페이지 layout 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import Link from 'next/link'
 
 const SplashPage = () => {
   return (
-    <div className="flex h-full flex-col px-mobile_safe">
-      <div className="flex w-full flex-1 flex-col gap-[70px] pt-[70px]">
+    <div className="flex flex-1 flex-col justify-between gap-6 px-mobile_safe">
+      <div className="flex size-full flex-1 flex-col gap-8 pt-10">
         <div className="relative aspect-square w-full">
-          <Image src={splashImage} alt="splash_bg" fill className="object-contain" sizes="100vw" />
+          <Image src={splashImage} alt="splash_bg" fill className="object-contain" />
         </div>
-        <div className="flex flex-1 flex-col items-center gap-[15px]">
+        <div className="flex flex-col items-center gap-[10px]">
           <p className="font-bmjua text-4xl font-bold">개발의민족</p>
           <p className="text-center text-base text-gray-500">
             맛있게 먹으면 0칼로리 입니다^^
@@ -18,7 +18,7 @@ const SplashPage = () => {
           </p>
         </div>
       </div>
-      <div className="pb-[80px]">
+      <div className="pb-[40px]">
         <Link href={ROUTE_PATHS.HOME}>
           <button className="h-bottom_navigation w-full rounded-[6px] bg-primary text-white">
             시작하기
