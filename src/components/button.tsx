92d2abf80d87b6b5e97ac146dbbbe349c287db3d
PNS212624\Hansol <yrlee00@hansol.com>
2025-01-09 14:14:46 +0900

    ✨ Feat:  버튼 공통 컴포넌트 사이즈 추가 개선

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,24 +1,27 @@
 import * as React from 'react'
-import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
 
-const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
-  {
-    variants: {
-      variant: {
-        default: 'bg-primary text-white hover:bg-primary/90 w-full h-10 px-4 py-2',
-        primaryFit: 'w-fit h-10 px-4 py-2 text-primary border-solid border-primary',
-        grayFit: 'w-fit h-10 px-4 py-2 text-gray-400 border-solid border-gray-400',
-      },
+const buttonVariants = cva('inline-flex items-center justify-center gap-2 whitespace-nowrap', {
+  variants: {
+    variant: {
+      default: 'bg-primary text-white hover:bg-primary/90 w-full px-4 py-2',
+      primaryFit: 'w-fit px-4 py-2 text-primary border-solid border-primary',
+      grayFit: 'w-fit px-4 py-2 text-gray-400 border-solid border-gray-400',
     },
-    defaultVariants: {
-      variant: 'default',
+
+    size: {
+      default: 'rounded-md text-base h-10',
+      s: 'rounded text-sm h-8',
+      m: 'rounded-md text-base h-12',
     },
   },
-)
+  defaultVariants: {
+    variant: 'default',
+    size: 'default',
+  },
+})
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -27,9 +30,10 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button'
-    return <Comp className={cn(buttonVariants({ variant, className }))} ref={ref} {...props} />
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    return (
+      <button className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+    )
   },
 )
 Button.displayName = 'Button'
