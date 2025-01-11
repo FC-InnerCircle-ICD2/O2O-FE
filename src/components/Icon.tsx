68f4d7596280fd1680a9570fb73abcdcb57e543f
박용석 <4up456@naver.com>
2025-01-11 14:32:07 +0900

    ✨ Feat:  Icon 추가(building, cog, credit-card, double-chat, spearkerphone)

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -3,15 +3,20 @@ import arrowLeft from '@/assets/icons/arrow-left.svg'
 import arrowNarrowDown from '@/assets/icons/arrow-narrow-down.svg'
 import arrowNarrowUp from '@/assets/icons/arrow-narrow-up.svg'
 import arrowUp from '@/assets/icons/arrow-up.svg'
+import building from '@/assets/icons/building.svg'
 import chat from '@/assets/icons/chat.svg'
 import clipboard from '@/assets/icons/clipboard.svg'
 import close from '@/assets/icons/close.svg'
+import cog from '@/assets/icons/cog.svg'
+import creditCard from '@/assets/icons/credit-card.svg'
 import dash from '@/assets/icons/dash.svg'
 import dotsHorizontal from '@/assets/icons/dots-horizontal.svg'
+import doubleChat from '@/assets/icons/double-chat.svg'
 import heart from '@/assets/icons/heart.svg'
 import home from '@/assets/icons/home.svg'
 import search from '@/assets/icons/search.svg'
 import share from '@/assets/icons/share.svg'
+import speakerphone from '@/assets/icons/speakerphone.svg'
 import star from '@/assets/icons/star.svg'
 import user from '@/assets/icons/user.svg'
 import warning from '@/assets/icons/warning.svg'
@@ -38,6 +43,11 @@ export const ICONS = {
   arrowNarrowDown,
   xCircle,
   chat,
+  speakerphone,
+  doubleChat,
+  creditCard,
+  cog,
+  building,
 }
 
 interface IconProps {
