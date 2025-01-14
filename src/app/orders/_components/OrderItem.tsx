10ceb957c5f279e6b994a99881895dccae3b44a2
PNS212624\Hansol <yrlee00@hansol.com>
2025-01-14 10:39:18 +0900

    ✨ Feat:  주문 내역 페이지 디자인 리뷰 후 수정 (badge 추가, separator 추가, 디자인 수정)

--- src/app/orders/_components/OrderItem.tsx
+++ src/app/orders/_components/OrderItem.tsx
@@ -1,29 +1,36 @@
 import Chip from '@/components/Chip'
 import Image from 'next/image'
 import { Button } from '@/components/button'
+import Badge from '@/components/Badge'
 
 const OrderItem = () => {
   return (
-    <div className="flex flex-col gap-3 border border-solid border-gray-400 p-3 px-mobile_safe rounded">
+    <div className="flex flex-col gap-3">
       <div className="flex flex-row items-center">
         <Image
-          className="size-[70px] rounded-xl object-cover object-center"
+          className="size-[85px] rounded-xl object-cover object-center"
           src={
             'https://flexible.img.hani.co.kr/flexible/normal/970/647/imgdb/resize/2017/0709/149948783091_20170709.JPG'
           }
           alt="음식점 대표 이미지"
-          width={70}
-          height={70}
+          width={85}
+          height={85}
           loading="lazy"
         />
-        <div className="flex flex-col gap-2 pl-3">
-          <div className="text-lg font-bold">빙동댕 {'>'}</div>
-          <div className="text-sm text-gray-700">새우 로제 파스타 외 2개 20,000원</div>
+        <div className="flex w-4/5 flex-col gap-2 pl-3">
+          <div className="flex flex-row justify-between">
+            <Badge variant="complete">배달완료</Badge>
+            <div className="text-xs text-gray-400">2025.01.11 오후 07:24</div>
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="truncate text-lg font-bold hover:text-clip">빙동댕</div>
+            <div className="text-sm text-gray-700">새우 로제 파스타 외 2개 20,000원</div>
+          </div>
         </div>
       </div>
       <div className="flex flex-row justify-center gap-7">
         <Button variant="primaryFit" size="s" className="w-1/2">
-          같은 메뉴 담기
+          주문 상세
         </Button>
         <Button variant="grayFit" size="s" className="w-1/2">
           리뷰 달기
--- src/app/orders/_components/OrderSearch.tsx
+++ src/app/orders/_components/OrderSearch.tsx
@@ -1,15 +1,22 @@
+'use client'
+
 import Input from '@/components/input'
 import Icon from '@/components/Icon'
-import { COLORS } from '@/styles/color'
+import { useState } from 'react'
 
 const OrderSearch = () => {
+  const [word, setWord] = useState('')
+
   return (
     <div className="mb-6 flex w-full flex-col gap-7">
       <div className="w-full bg-white">
         <Input
-          type="search"
           placeholder="주문 내역을 검색하세요"
-          icon={<Icon name="Search" size={20} color={COLORS.gray400} />}
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          onReset={() => setWord('')}
+          icon={<Icon name="Search" size={18} />}
+          offOutline
         />
       </div>
     </div>
