f13e4e9249c796ce0055679e498b0524e214b34a
박용석 <4up456@naver.com>
2025-01-15 11:30:16 +0900

    ✨ Feat:  Separator 컴포넌트 개선

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,8 +1,26 @@
+import { cn } from '@/lib/utils'
 import { SeparatorProps } from '@radix-ui/react-separator'
 import { Separator as ShadcnSeparator } from './shadcn/separator'
 
-const Separator = ({ ...props }: SeparatorProps) => {
-  return <ShadcnSeparator {...props} />
+type SeparatorType = SeparatorProps & {
+  ignoreMobileSafe?: boolean
+}
+
+/**
+ * @param ignoreMobileSafe - 모바일 안전영역을 무시할지 여부 (horizontal일 때만 동작) (기본값: false)
+ * @param orientation - 방향을 설정합니다. "horizontal" | "vertical" (기본값: "horizontal")
+ * @param className - 구분선의 너비(horizontal일 때는 h-[너비], vertical일 때는 w-[너비])와 색상(bg-[색상])를 조정합니다.
+ */
+const Separator = ({ ignoreMobileSafe = false, ...props }: SeparatorType) => {
+  return (
+    <ShadcnSeparator
+      {...props}
+      className={cn(
+        props.className,
+        ignoreMobileSafe && props.orientation === 'horizontal' && 'ignore-mobile-safe',
+      )}
+    />
+  )
 }
 
 export default Separator
