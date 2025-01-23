ec7b19ea35516784ed82777e039515110a886a2b
PNS212624\Hansol <yrlee00@hansol.com>
2025-01-23 11:20:46 +0900

    ✨ Feat:  주문하기 페이지 구현 완료

--- src/app/pay/_components/OrderInfo.tsx
+++ src/app/pay/_components/OrderInfo.tsx
@@ -3,10 +3,11 @@ import Separator from '@/components/Separator'
 import MenuItem from '@/app/pay/_components/MenuItem'
 import Input from '@/components/input'
 import { Checkbox } from '@/components/shadcn/checkbox'
+import { Button } from '@/components/button'
 
 const OrderInfo = () => {
   return (
-    <div className="flex flex-col gap-5">
+    <div className="mb-5 flex flex-col gap-5">
       <div className="flex flex-row justify-between">
         <div className="flex flex-row gap-2">
           <Icon name="Bike" size={24} />
@@ -49,7 +50,7 @@ const OrderInfo = () => {
           <div className="font-bold">메뉴 추가하기</div>
         </div>
       </div>
-      <div className="flex flex-col gap-3 rounded-xl border border-solid border-gray-400 p-5">
+      <div className="flex flex-col gap-4 rounded-xl border border-solid border-gray-400 p-5">
         <div className="text-base font-extrabold">가게 요청사항</div>
         <Input placeholder="예) 견과류 빼주세요" />
         <div className="items-top flex space-x-2">
@@ -59,14 +60,44 @@ const OrderInfo = () => {
               htmlFor="terms1"
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
-              Accept terms and conditions
+              일회용 수저, 포크는 빼주세요
             </label>
-            <p className="text-sm text-muted-foreground">
-              You agree to our Terms of Service and Privacy Policy.
-            </p>
+          </div>
+        </div>
+        <Separator />
+        <div className="flex flex-row justify-between">
+          <div className="place-content-center text-base font-extrabold">라이더 요청사항</div>
+          <div className="flex flex-row gap-1">
+            <div className="place-content-center text-sm">요청사항 없음</div>
+            <Icon name="ChevronRight" size={24} />
           </div>
         </div>
       </div>
+      <div className="flex flex-col gap-4 rounded-xl border border-solid border-gray-400 p-5">
+        <div className="flex flex-row justify-between">
+          <div className="place-content-center text-base font-extrabold">결재수단</div>
+          <div className="flex flex-row gap-1">
+            <div className="place-content-center text-sm text-primary">결재수단을 선택해주세요</div>
+            <Icon name="ChevronRight" size={24} />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 px-1">
+        <div className="flex flex-row justify-between">
+          <div>상품금액</div>
+          <div>24,500원</div>
+        </div>
+        <div className="flex flex-row justify-between">
+          <div>배달요금</div>
+          <div>0원</div>
+        </div>
+      </div>
+      <Separator />
+      <div className="flex flex-row justify-between px-1">
+        <div className="text-lg font-bold">총 결재금액</div>
+        <div className="text-lg font-bold">27,500원</div>
+      </div>
+      <Button>27,500원 배달 결제하기</Button>
     </div>
   )
 }
