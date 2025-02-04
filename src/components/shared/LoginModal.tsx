119e4e5aa20026b1970f44ab31dcb8fb9182f3ed
박용석 <4up456@naver.com>
2025-02-04 23:26:51 +0900

    ✨ Feat:  로그인 로직 구현

--- src/components/shared/LoginModal.tsx
+++ src/components/shared/LoginModal.tsx
@@ -1,9 +1,12 @@
+import usePostLogin from '@/api/usePostLogin'
 import { Button } from '@/components/button'
 import Icon from '@/components/Icon'
 import Input from '@/components/Input'
 import Separator from '@/components/Separator'
 import SignupModal from '@/components/shared/SignupModal'
+import { useToast } from '@/hooks/useToast'
 import { modalStore } from '@/store/modal'
+import { HTTPError } from 'ky'
 import { useForm } from 'react-hook-form'
 
 const LoginModal = () => {
@@ -42,27 +45,40 @@ const LoginForm = () => {
   const { hideModal } = modalStore()
   const { register, handleSubmit, watch } = useForm({
     defaultValues: {
-      email: '',
+      signname: '',
       password: '',
     },
   })
 
-  const emailValue = watch('email')
+  const signnameValue = watch('signname')
   const passwordValue = watch('password')
-  const isButtonDisabled = !emailValue || !passwordValue
+  const isButtonDisabled = !signnameValue || !passwordValue
+  const { mutate: login } = usePostLogin()
+  const { toast } = useToast()
+
 
   const onSubmit = handleSubmit((formData) => {
-    // TODO: 로그인 로직 추가
-    console.log('Form submitted:', formData)
-    // TODO: 로그인 실패 시 에러 토스트 띄우기
-    // 로그인 성공 시 모달 닫기
-    hideModal()
+    login(formData, {
+      onSuccess: () => {
+        hideModal()
+      },
+      onError: async (error: Error) => {
+        const httpError = error as HTTPError
+        const errorData = await httpError.response?.json() as { data: { error: string } }
+        toast({
+          title: "로그인 실패",
+          description: errorData?.data.error || "로그인에 실패했습니다.",
+          variant: "destructive",
+          position: "center",
+        })
+      }
+    })
   })
 
   return (
     <form onSubmit={onSubmit}>
       <div className="mb-3">
-        <Input placeholder="이메일 주소 입력" {...register('email')} offOutline />
+        <Input placeholder="이메일 주소 입력" {...register('signname')} offOutline />
       </div>
       <div className="mb-8">
         <Input type="password" placeholder="비밀번호 입력" {...register('password')} offOutline />
