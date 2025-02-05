a42f74720e4612b0f734e746f7d3a1b2a66264a7
박용석 <4up456@naver.com>
2025-02-05 14:14:58 +0900

    ✨ Feat:  auth 관련 타입 지정

---
+++ src/models/auth.ts
@@ -0,0 +1,25 @@
+export interface SignupData {
+  signname: string
+  password: string
+  nickname: string
+}
+
+export interface LoginData {
+  signname: string
+  password: string
+}
+
+export interface LoginResponse {
+  accessToken: string
+  refreshToken: string
+  accessTokenExpiresIn: string
+  refreshTokenExpiresIn: string
+}
+
+
+export interface Member {
+  id: number
+  signname: string
+  nickname: string
+  profileImage: string
+}
