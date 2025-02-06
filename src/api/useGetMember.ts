be1f6260bfa4e1970f3de50c9b11f0f83c1a49b7
박용석 <4up456@naver.com>
2025-02-06 19:58:04 +0900

    🐛 Fix:  불필요한 코드 삭제

--- src/api/useGetMember.ts
+++ src/api/useGetMember.ts
@@ -3,19 +3,6 @@ import { api } from '@/lib/api';
 import { Member } from '@/models/auth';
 import { useQuery } from '@tanstack/react-query';
 
-interface StoreDetail {
-  address: string
-  deliveryTime: string
-  freeDelivery: boolean
-  id: number
-  imageMain: string
-  latitude: number
-  longitude: number
-  name: string
-  phone: string
-  rating: number
-  reviewCount: number
-}
 
 const useGetMember = () => {
     const accessToken = useLocalStorage('accessToken')
