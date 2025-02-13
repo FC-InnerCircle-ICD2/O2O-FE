7f63a03268077c350179296d9e9ffe3ca97adbb7
PNS212624\Hansol <yrlee00@hansol.com>
2025-02-13 17:01:57 +0900

    ✨ Feat:  카카오 map pin 연동

--- src/app/mypage/address/detail/_components/KakaoMap.tsx
+++ src/app/mypage/address/detail/_components/KakaoMap.tsx
@@ -1,11 +1,18 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { Map } from 'react-kakao-maps-sdk'
+import { Map, MapMarker } from 'react-kakao-maps-sdk'
+
+const center = {
+  // 지도의 중심좌표
+  lat: 33.450701,
+  lng: 126.570667,
+}
 
 const KakaoMap = () => {
   const apiKey: string | undefined = process.env.NEXT_PUBLIC_KAKAO_APP_KEY
   const [scriptLoad, setScriptLoad] = useState<boolean>(false)
+  const [position, setPosition] = useState<{ lat: number; lng: number }>()
 
   useEffect(() => {
     const script: HTMLScriptElement = document.createElement('script')
@@ -18,17 +25,32 @@ const KakaoMap = () => {
     })
   }, [])
   return (
-    <div>
-      {scriptLoad ? (
-        <Map
-          center={{ lat: 33.5563, lng: 126.79581 }}
-          style={{ width: '100%', height: '350px' }}
-          level={3}
-        ></Map>
-      ) : (
-        <div>1</div>
-      )}
-    </div>
+    <>
+      <Map // 지도를 표시할 Container
+        id="map"
+        center={center}
+        style={{
+          width: '100%',
+          height: '350px',
+        }}
+        level={3} // 지도의 확대 레벨
+        onClick={(_, mouseEvent) => {
+          const latlng = mouseEvent.latLng
+          setPosition({
+            lat: latlng.getLat(),
+            lng: latlng.getLng(),
+          })
+        }}
+      >
+        <MapMarker position={position ?? center} />
+      </Map>
+      {/*<p>*/}
+      {/*  <em>지도를 클릭해주세요!</em>*/}
+      {/*</p>*/}
+      {/*<div id="clickLatlng">*/}
+      {/*  {position && `클릭한 위치의 위도는 ${position.lat} 이고, 경도는 ${position.lng} 입니다`}*/}
+      {/*</div>*/}
+    </>
   )
 }
 
