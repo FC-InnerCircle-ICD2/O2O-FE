581763beea1e1c8a8691a8ca296c5f2bfcb58d82
yeonhee7935 <yeonhee7935@naver.com>
2025-02-17 21:03:09 +0900

    Feat: floating cart button

--- src/api/useGetCarts.ts
+++ src/api/useGetCarts.ts
@@ -1,19 +1,40 @@
 import { api } from '@/lib/api'
-import { useQuery } from '@tanstack/react-query'
-interface Cart {
-  id: number
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+interface CartItem {
+  cartId: number
+  menuId: string
   name: string
-  price: number
+  totalPrice: number
   quantity: number
   imageUrl: string
-  stock: number
-  selected: boolean
+  orderMenuOptionGroups: {
+    id: string
+    name: string
+    orderMenuOptionIds: {
+      id: string
+      name: string
+    }[]
+  }[]
 }
+export interface Cart {
+  storeId: string
+  orderMenus: CartItem[]
+}
+
 const useGetCarts = () => {
-  return useQuery({
+  const qc = useQueryClient()
+
+  const { data: carts } = useQuery({
     queryKey: ['carts'],
-    queryFn: async () => await api.get<Cart[]>(`carts`, {}),
-    enabled: false,
+    queryFn: async () => await api.get<Cart>(`carts`, {}),
+    enabled: true,
   })
+
+  const resetCarts = () => {
+    qc.refetchQueries({ queryKey: ['carts'] })
+  }
+
+  return { carts, resetCarts }
 }
+
 export default useGetCarts
---
+++ src/components/CartButton.tsx
@@ -0,0 +1,59 @@
+import useGetCarts from '@/api/useGetCarts'
+import { cn } from '@/lib/utils'
+import { ROUTE_PATHS } from '@/utils/routes'
+import Link from 'next/link'
+import { useMemo } from 'react'
+import Icon from './Icon'
+
+interface CartButtonProps {
+  hasBottomNavigation?: boolean
+  className?: string
+}
+
+const CartButton = ({ className = '', hasBottomNavigation = true }: CartButtonProps) => {
+  const { carts } = useGetCarts()
+  const cartItems = useMemo(() => carts?.orderMenus.length || 0, [carts])
+
+  // 배지 위치
+  const getBadgePosition = (count: number) => {
+    if (count < 10) return { top: '-top-4', right: '-right-4' }
+    if (count < 100) return { top: '-top-5', right: '-right-5' }
+    return { top: '-top-6', right: '-right-6' }
+  }
+
+  // 100 이상이면 "99+"로 변환
+  const formatCartCount = (count: number) => (count >= 100 ? '99+' : count)
+
+  const { top, right } = getBadgePosition(cartItems)
+  const formattedCartItems = formatCartCount(cartItems)
+
+  if (cartItems === 0) return <></>
+
+  return (
+    <Link href={ROUTE_PATHS.PAY}>
+      <div
+        className={cn(
+          'hover:bg-primary-dark border-primary-300 fixed bottom-[5rem] right-5 z-50 cursor-pointer rounded-full border border-solid border-primary-foreground bg-primary p-[12px] transition-colors',
+          'shadow-[0_4px_6px_rgba(var(--color-primary-rgb),0.5)]', // primary 색상의 그림자 추가
+          hasBottomNavigation && 'bottom-[4.5rem]',
+          className
+        )}
+      >
+        <div className="relative">
+          <Icon name="ShoppingCart" size={20} color="white" />
+          <div
+            className={cn(
+              'absolute flex aspect-square min-h-[1.5rem] min-w-[1.5rem] items-center justify-center rounded-full border-2 border-solid border-primary bg-white p-1 text-xs font-bold text-primary',
+              top,
+              right
+            )}
+          >
+            {formattedCartItems}
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default CartButton
