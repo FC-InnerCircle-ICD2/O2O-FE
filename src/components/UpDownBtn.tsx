c1fbca32f0f310f0b77affbad34a2389daf80852
yeonhee7935 <yeonhee7935@naver.com>
2025-02-22 12:12:02 +0900

    ♻️ Refactor:  장바구니 담기 로직 수정

--- src/components/UpDownBtn.tsx
+++ src/components/UpDownBtn.tsx
@@ -11,11 +11,11 @@ const UpDownBtn = ({ value = 0, onChange }: UpDownProps) => {
   return (
     <div className="flex h-8 flex-row items-center rounded-lg border border-solid border-gray-300 px-2">
       <button className="flex items-center justify-center" onClick={() => onChange(value - 1)}>
-        <Icon name="Minus" size={18} color={value <= 1 ? 'gray' : 'black'} />
+        <Icon name="Minus" size={18} color={value <= 1 ? '#d3d3d3' : 'black'} />
       </button>
       <div className="mx-2 flex w-4 items-center justify-center text-sm font-bold">{value}</div>
       <button className="flex items-center justify-center" onClick={() => onChange(value + 1)}>
-        <Icon name="Plus" size={18} color={value >= 99 ? 'gray' : 'black'} />
+        <Icon name="Plus" size={18} color={value >= 99 ? '#d3d3d3' : 'black'} />
       </button>
     </div>
   )
