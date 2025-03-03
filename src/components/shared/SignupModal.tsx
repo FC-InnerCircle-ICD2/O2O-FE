73e078187082b331510e487eb0bcb7890b16e1cf
박용석 <4up456@naver.com>
2025-03-03 21:08:23 +0900

    🎨 Design:  주소 상세 화면은 모달 등장 애니메이션 삭제

--- src/components/shared/SignupModal.tsx
+++ src/components/shared/SignupModal.tsx
@@ -350,8 +350,6 @@ const AddressModal = ({
               onSaveInSignup={onSaveInSignup}
             />
           ),
-          useAnimation: true,
-          useDimmedClickClose: true,
         })
       },
       onError: (error) => {
